dd9cb7cfecded8d3c58b8d23e020873b58ed58c3
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-12-16 00:53:32 +0200

    change API

--- src/servise/apiImg.js
+++ src/servise/apiImg.js
@@ -1,16 +1,10 @@
 const API_KEY = "22603097-01ea7c9e46d89c9af2e821f90";
 const BASE_URL = "https://pixabay.com/api/";
 
-function fetchImg(searchQuery, pages) {
+function fetchImg(searchQuery, page) {
   return fetch(
-    `${BASE_URL}?q=${searchQuery}&page=${pages}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-  ).then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-
-    return Promise.reject(new Error(`No images on request`));
-  });
+    `${BASE_URL}?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+  );
 }
 
-export { fetchImg };
+export default fetchImg;
