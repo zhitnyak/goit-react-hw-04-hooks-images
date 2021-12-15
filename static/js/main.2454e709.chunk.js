3db96f709916adb02d88299bcfef65423e658cf6
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-12-16 00:58:32 +0200

    Updates

--- static/js/main.2454e709.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-04-hooks-images"]=this["webpackJsonpgoit-react-hw-04-hooks-images"]||[]).push([[0],{11:function(t,e,n){"use strict";n.r(e);var c=n(1),o=n.n(c),s=n(3),i=n.n(s),r=(n(8),n(9),n(0));var a=function(){return Object(r.jsx)("div",{className:"App"})};i.a.render(Object(r.jsx)(o.a.StrictMode,{children:Object(r.jsx)(a,{})}),document.getElementById("root"))},8:function(t,e,n){},9:function(t,e,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.2454e709.chunk.js.map
